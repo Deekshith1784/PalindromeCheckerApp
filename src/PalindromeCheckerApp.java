a02f578738b98bb9b843136aa7ee2c76f9b89d2e
Deekshith1784 <deekshith1784rgmail.com>
2026-02-16 11:06:19 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+
+
+
+
+
+public class PalindromeCheckerApp {
+    public static void main(String[]args){
+        System.out.println("PalindromeCheckerApp");
+        System.out.println("version : 1.0");
+        System.out.println("system intialized successfully");
+
+    }
+}
