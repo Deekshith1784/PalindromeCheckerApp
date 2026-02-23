4d1ac642f22a08efe8274713320b8f5274024dc6
gowtham <gs7318@srmist.edu.in>
2026-02-23 10:57:54 +0530

    UC3 welcome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PalindroneCheckerApp {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
